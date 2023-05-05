1f0d2a4f4583a3b5786064d084da9e2a79782870
Michael Mathias <michaelscodingjourney@gmail.com>
2023-05-05 11:50:28 -0700

    Added compatible platforms to db.json

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const fromSoftwareGames = document.getElementById('fs-games');
-const detailImage = document.getElementById('detail-image');
+const gameImage = document.getElementById('detail-image');
 const gameTitle = document.getElementById('game-title');
 const gameRating = document.getElementById('game-rating');
 const gameRelease = document.getElementById('game-release');
+const gameReview = document.getElementById('game-review');
+const gamePlatforms = document.getElementById('game-platforms');
 
 fetch('http://localhost:3000/games')
 .then(response => response.json())
 .then(data => {
-    detailImage.src = data[0].image;
+    gameImage.src = data[0].image;
+    gameTitle.textContent = data[0].title;
+    gameRating.textContent = data[0].rating;
+    gameRelease.textContent = data[0].release;
+    gamePlatforms.textContent = data[0].platforms;
 
     data.forEach(game => {
         const img = document.createElement('img');
@@ -15,12 +21,12 @@ fetch('http://localhost:3000/games')
         img.src = game.image;
 
         img.addEventListener('click', e => {
-            detailImage.src = game.image;
+            gameImage.src = game.image;
             gameTitle.textContent = game.title;
             gameRating.textContent = game.rating;
             gameRelease.textContent = game.release;
+            gamePlatforms.textContent = game.platforms;
         })
-
         fromSoftwareGames.appendChild(img);
     });
 })
